c2c593219893f7775ec45ae390e2066376f8b91c
BurakDorman <burakdorman@gmail.com>
2025-06-15 23:12:28 +0300

    update

--- app.config.js
+++ app.config.js
@@ -69,7 +69,8 @@ export default {
       ]
     ],
     experiments: {
-      typedRoutes: true
+      typedRoutes: true,
+      tsconfigPaths: true
     },
     extra: {
       router: {
--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,16 @@ const { getDefaultConfig } = require("expo/metro-config");
 const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+// Add support for importing from the app directory
 config.resolver.alias = {
   "@": path.resolve(__dirname), // <-- now "@/components/styles" → "<root>/components/styles"
 };
+
+// Add support for importing from the app directory
+config.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', 'json'];
+
+// Add support for importing from the app directory
+config.watchFolders = [path.resolve(__dirname, 'app')];
 
 module.exports = config;
