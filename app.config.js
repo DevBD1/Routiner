60dae9bc044d7dad1054e35d3a6bf32d69a5c8d6
BurakDorman <burakdorman@gmail.com>
2025-06-15 17:42:53 +0300

    fully ported to dynamic app config js

--- app.config.js
+++ app.config.js
@@ -2,13 +2,15 @@ export default {
   expo: {
     name: "Routiner",
     slug: "routiner",
-    scheme: "routiner",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
-    userInterfaceStyle: "light",
+    scheme: "routiner",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
     splash: {
-      image: "./assets/images/splash-icon.png",
+      image: "./assets/images/logo-no-background.png",
+      imageWidth: 200,
       resizeMode: "contain",
       backgroundColor: "#ffffff"
     },
@@ -18,19 +20,64 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.devbd1.routiner",
-      ITSAppUsesNonExemptEncryption: false
+      googleServicesFile: "./GoogleService-Info.plist",
+      ITSAppUsesNonExemptEncryption: false,
+      infoPlist: {
+        CFBundleURLTypes: [
+          {
+            CFBundleURLSchemes: [
+              "routiner"
+            ]
+          }
+        ]
+      }
     },
     android: {
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#ffffff"
       },
-      package: "com.devbd1.routiner"
+      package: "com.devbd1.routiner",
+      googleServicesFile: "./google-services.json"
     },
     web: {
+      bundler: "metro",
+      output: "static",
       favicon: "./assets/images/favicon.png"
     },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/logo-no-background.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff"
+        }
+      ],
+      "@react-native-firebase/app",
+      "@react-native-firebase/auth",
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            useFrameworks: "static",
+            modular_headers: true
+          }
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    },
     extra: {
+      router: {
+        origin: false
+      },
+      eas: {
+        projectId: "351b6748-97e9-4918-a5ce-867db8edeb15"
+      },
       firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
       firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
       firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
@@ -39,9 +86,12 @@ export default {
       firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
       firebaseMeasurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
       googleWebClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
-      eas: {
-        projectId: "351b6748-97e9-4918-a5ce-867db8edeb15"
+      google: {
+        androidClientId: process.env.GOOGLE_ANDROID_CLIENT_ID,
+        iosClientId: process.env.GOOGLE_IOS_CLIENT_ID,
+        webClientId: process.env.GOOGLE_WEB_CLIENT_ID
       }
-    }
+    },
+    owner: "devbd1"
   }
 }; 
